66436759bee3ce7bce1683dcc6fe889e920f29d4
Art <wildart@gmail.com>
2017-10-14 11:37:19 -0400

    add static routes

--- settings.js
+++ settings.js
@@ -2,6 +2,7 @@ require('dotenv').config({silent: true});
 
 module.exports = {
   port: process.env.PORT || 3000,
+  chat_port: process.env.CHAT_PORT || 3001,
   env: process.env.ENV || 'development',
 
   // Environment-dependent settings
